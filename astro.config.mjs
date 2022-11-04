5c6ec69829caf56347d94376f3c3ae236288c0ea
Brian Hinton <briannhinton@gmail.com>
2022-11-04 18:46:59 +0000

    feat: built out components and pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
